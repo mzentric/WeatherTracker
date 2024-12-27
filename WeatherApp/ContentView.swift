3fe2f66c9646d3714cbfedce24b0f6ee916803b4
mzentric <mzentric@users.noreply.github.com>
2024-12-27 12:12:35 -0500

    initial checkin

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                TextField("Search Location", text: $searchText)
+                    .textFieldStyle(.plain)
+                    .padding(10)
+                    .padding(.horizontal, 25)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(15)
+                    .overlay(
+                        HStack {
+                            Spacer()
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                                .padding(.trailing, 8)
+                        }
+                    )
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            Spacer()
+            
+            VStack(spacing: 10) {
+                Text("No City Selected")
+                    .font(.title)
+                    .fontWeight(.medium)
+                Text("Please Search For A City")
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
